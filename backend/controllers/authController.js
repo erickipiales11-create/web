918d7a6cf13abbd54fd6304de1df6a9b09dbd8ad
erickipiales11-create <erickipiales11@gmail.com>
2026-06-10 15:02:20 -0500

    controlador agregado

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,3 +3,45 @@ import jwt from 'jsonwebtoken';
 import dotnet from 'dotenv';
 
 import User from '../models/User.js';
+
+dotnet.config();
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const exist = await User.findOne({ where: { email } });
+        
+        if (exist)
+            return res.status(500).json({ message: "el usuario ya existe" });
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({ name, email, password: hashedPassword });
+
+        return res.status(201).json({ message: "usuario creado", user:{ 
+            id: user.id, name: user.name, email: user.email }
+         });
+        } catch (error) {
+            return res.status(500).json({ message: "error" });
+        }
+        }
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user)
+            return res.status(401).json({ message: "credenciales invalidas" });
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword)
+            return res.status(401).json({ message: "credenciales invalidas" });
+        const token = jwt.sign({ 
+            id: user.id, 
+            email: user.email }, process.env.JWT_SECRET, { expiresIn: '24h' });
+            return res.status(201).json({ message: "usuario autenticado", token });
+
+        } catch (error) {
+            return res.status(500).json({ message: error });
+
+        }
+}
